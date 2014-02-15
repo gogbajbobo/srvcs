3650be4466b607be68d54dc23ebb9a767afe2474
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-02-15 15:40:05 +0400

    somewhere in the middle

--- doi.js
+++ doi.js
@@ -20,7 +20,11 @@ function insert_form() {
 			'DOI:',
 			$('<input />').attr({
 				type: 'text',
-				name: 'q'
+				name: 'q',
+				autofocus: 'autofocus',
+// 
+				value: '10.1063/1.1332401'
+// 
 			})
 		),
 		$('<input />').attr({
@@ -60,7 +64,7 @@ function send_form(form) {
 			if (data.totalResults != 1) {
 				$('span.info').text('wrong doi, totalResults: ' + data.totalResults);
 			} else {
-				$('span.info').text('items: ' + data.items[0].doi);
+				show_result_form(data);
 			}
 
 		}
@@ -77,3 +81,109 @@ function send_form(form) {
 		});
 
 }
+
+function show_result_form(data) {
+
+	$('body').empty();
+	form = $('<form />').attr('id', 'result_form');
+	$('body').append(form);
+	$(form).append(result_inputs(data));
+
+}
+
+function result_inputs(data) {
+
+	var item = data.items[0];
+	var coins = parse_coins(item.coins);
+	var inputs =
+		[
+			$('<label />').append(
+				'Doi: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: item.doi
+				})
+			),
+
+			$('<label />').append(
+				'Article title: ' + data.items[0].coins,
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].coins
+				})
+			),
+
+			$('<label />').append(
+				'Journal title: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].doi
+				})
+			),
+
+			$('<label />').append(
+				'Date: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].doi
+				})
+			),
+
+			$('<label />').append(
+				'Genre: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].doi
+				})
+			),
+
+			$('<label />').append(
+				'Authors: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].doi
+				})
+			),
+
+			$('<label />').append(
+				'Number of authors: ',
+				$('<input />').attr({
+					type: 'text',
+					name: 'doi',
+					value: data.items[0].doi
+				})
+			),
+
+			$('<input />').attr({
+				type: 'submit',
+				value: 'OK',
+				autofocus: 'autofocus'
+			})
+		]
+
+	return inputs;
+
+}
+
+function parse_coins(coins) {
+
+	var result_array;
+	var coins_array = coins.split('&');
+	console.log(coins_array[0]);
+	coins_array.each(function( index ) {
+  console.log( index + ": " + $( this ).text() );
+});
+
+	// coins_array.foreach(function(name){
+	// 	console.log('test');
+	// });
+
+}
+
+
